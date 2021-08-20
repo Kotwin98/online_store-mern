b1333e6ad5e89da858e50d5e9d6ff436f30931fc
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-08-20 08:37:20 +0200

    Add a product controller

---
+++ backend/controllers/product-controllers.js
@@ -0,0 +1,12 @@
+const Product = require('../models/Product');
+
+const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.json(products);
+    } catch {
+        throw new Error('Sorry, we have a server error');
+    }
+}
+
+module.exports = getProducts;
