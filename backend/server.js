ca17f44bb36b42d69111e3dafb2686881d7726f2
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-08-20 09:56:52 +0200

    Change code

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
+const productRoutes = require('./routes/products-routes'); 
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-})
+app.use('/products', productRoutes);
