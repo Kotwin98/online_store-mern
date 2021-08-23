c823105d635ab60459868c245c5c6a2af6e09ddb
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-08-23 18:29:50 +0200

    Add dummy data for testing

---
+++ backend/data/products.js
@@ -0,0 +1,10 @@
+const products = [
+    {
+        name: 'Smartphone',
+        description: 'A dummy product',
+        price: 200,
+        image: 'https://images.pexels.com/photos/4065906/pexels-photo-4065906.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+    }
+];
+
+module.exports = products;
