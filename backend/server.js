9f65813f96ba73604edd6feff2d279b024a7589f
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-11-20 10:02:57 +0100

    change endpoint

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ connectDB();
 
 const app = express();
 
-app.use('/products', productRoutes);
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
