fd8f300c29c81c7d0ca9f303ecbcc29948da34a7
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-11-23 20:15:07 +0100

    replace component state with redux state

--- frontend/src/components/ProductList/ProductList.js
+++ frontend/src/components/ProductList/ProductList.js
@@ -1,52 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import SingleProduct from '../SingleProduct/SingleProduct';
 import './ProductList.css';
 
-const DUMMY__PRODUCTS = [
-    {
-        id: 0,
-        image: 'https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-        name: 'Product name 1',
-        price: 100,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper odio eu augue pellentesque tincidunt. In erat est, elementum vitae gravida at, ullamcorper non nunc.'
-    },
-    {
-        id: 1,
-        image: 'https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-        name: 'Product name 2',
-        price: 100,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper odio eu augue pellentesque tincidunt. In erat est, elementum vitae gravida at, ullamcorper non nunc.'
-    },
-    {
-        id: 2,
-        image: 'https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-        name: 'Product name 3',
-        price: 100,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper odio eu augue pellentesque tincidunt. In erat est, elementum vitae gravida at, ullamcorper non nunc.'
-    },
-    {
-        id: 3,
-        image: 'https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-        name: 'Product name 4',
-        price: 100,
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer semper odio eu augue pellentesque tincidunt. In erat est, elementum vitae gravida at, ullamcorper non nunc.'
-    }
-]
+import { getProducts as listProducts } from '../../redux/actions/productActions';
 
-const ProductList = props => {
+const ProductList = () => {
+    const dispatch = useDispatch();
+
+    const getProducts = useSelector(state => state.getProducts);
+    const { products, loading, error } = getProducts;
+
+    useEffect(() => {
+        dispatch(listProducts());
+    }, [dispatch])
+
     return (
         <div className="product-list__wrap">
-            {DUMMY__PRODUCTS.map(el => {
-                return (
-                    <SingleProduct 
-                        image={el.image}
-                        name={el.name}
-                        price={el.price}
-                        desc={el.desc}
-                    />
-                )
-            })}
+            {loading ? <h1>Loading...</h1> : error ? <h1>{error}</h1> : products.map(product => (
+                <SingleProduct 
+                    key={product._id} 
+                    id={product._id} 
+                    imageUrl={product.image}
+                    name={product.name}
+                    price={product.price}
+                    desc={product.description}
+                />
+            ))}
         </div>
     );
 }
