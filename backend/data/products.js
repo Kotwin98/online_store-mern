9fbfd091e8e3d061a4357d620fb648b2b77c7ef6
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-11-27 09:17:33 +0100

    Add more dummy products

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,9 +1,33 @@
 const products = [
     {
         name: 'Smartphone',
-        description: 'A dummy product',
+        description: 'A dummy smartphone description',
         price: 200,
         image: 'https://images.pexels.com/photos/4065906/pexels-photo-4065906.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+    },
+    {
+        name: 'Laptop',
+        description: 'A dummy laptop description',
+        price: 800,
+        image: 'https://images.pexels.com/photos/812264/pexels-photo-812264.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+    },
+    {
+        name: 'Headphones',
+        description: 'A dummy headphones description',
+        price: 600,
+        image: 'https://images.pexels.com/photos/3945667/pexels-photo-3945667.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+    },
+    {
+        name: 'Camera',
+        description: 'A dummy camera description',
+        price: 1000,
+        image: 'https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+    },
+    {
+        name: 'Tablet',
+        description: 'A dummy tablet description',
+        price: 400,
+        image: 'https://images.pexels.com/photos/1334597/pexels-photo-1334597.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
     }
 ];
 
