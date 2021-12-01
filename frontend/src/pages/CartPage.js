88bfff76321bbe245fe9f835a5e656b80feca109
Mateusz Kotwiński <mkotwinski98@gmail.com>
2021-12-01 06:35:05 +0100

    Add removeFromCartHandler

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -2,12 +2,17 @@ import React from 'react';
 import CartItem from '../components/CartItem/CartItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { removeFromCart } from '../redux/actions/cartActions';
 
 const CartPage = () => {
     const dispatch = useDispatch();
 
     const cart = useSelector(state => state.cart);
     const { cartItems } = cart;
+
+    const removeFromCartHandler = id => {
+        dispatch(removeFromCart(id));
+    }
     return (
         <div>
             <h1>Cart page</h1>
@@ -16,7 +21,7 @@ const CartPage = () => {
                     You don't have anything in your cart yet <Link to="/">Go Back</Link>
                 </div>
             ) : cartItems.map(item => (
-                <CartItem item={item} />
+                <CartItem key={item.product} item={item} removeFromCart={removeFromCartHandler} />
             ))}
         </div>
     )
